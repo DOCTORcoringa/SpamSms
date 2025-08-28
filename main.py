f82c305d440a2774f8ffd5d75906790a92790dec
Doctor coringa lunático <devanilsonfrancisco36@gmail.com>
2025-08-28 01:23:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class Main:
 		;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 		;       S P A M  S M S      ;
 		;---------------------------;
-		;       Author : noobie     ;
+		;       Author : Doctor     ;
 		;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 NOTE: This tool's only work for Indonesia number phone.
@@ -51,3 +51,4 @@ except KeyboardInterrupt:
 	exit('[Exit] Key interrupt')
 except Exception as F:
 	print('Err: %s'%(F))
+
